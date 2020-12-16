43e01dc9c8ce7abd25311a5d01ac05aa414ac571
maryna <merry.kosiv@gmail.com>
2020-12-16 15:33:36 +0200

    init

---
+++ src/data.js
@@ -0,0 +1,42 @@
+const reviews = [
+    {
+        id: 1,
+        name: 'susan smith',
+        job: 'web developer',
+        image:
+            'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+        text:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce feugiat fermentum sodales. Suspendisse' +
+            ' fringilla mi vel mollis gravida. Vestibulum eleifend ante ut mauris tempor.',
+    },
+    {
+        id: 2,
+        name: 'anna johnson',
+        job: 'web designer',
+        image:
+            'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg',
+        text:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam at mattis nulla, sed convallis ante. Maecenas dictum, augue nec ornare viverra, metus turpis ornare ipsum.',
+    },
+    {
+        id: 3,
+        name: 'peter jones',
+        job: 'intern',
+        image:
+            'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+        text:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pretium dolor in ex hendrerit viverra. Nulla commodo dignissim enim, sit amet consequat metus finibus ac.',
+    },
+    {
+        id: 4,
+        name: 'bill anderson',
+        job: 'the boss',
+        image:
+            'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+        text:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tempor, neque in sagittis volutpat, dolor massa tempor mauris, nec sagittis felis diam eu purus. Nullam.',
+    },
+];
+
+export default reviews;
+
